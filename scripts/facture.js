abba488d5d704352a715e3d545bef507e7147474
clementdroillard <droillard.clement@neuf.fr>
2018-05-27 01:47:31 +0200

    mouvement stock vente

--- scripts/facture.js
+++ scripts/facture.js
@@ -1,13 +1,13 @@
 setTimeout(chargerTableArticle, 1000);
 setTimeout(chargerTablePrestation, 1000);
 
+var tableArticleSelectionne = [];
+var tablePrestationSelectionne = [];
 
 //fonction de chargement de la table des articles
 function chargerTableArticle(){
     let dataTable = $('#tableArticle').DataTable();
     let xhr = new XMLHttpRequest();
-    //cache la colonne id
-    dataTable.column(0).visible(false);
 
 
     xhr.open("GET", api+"article/salon/"+salonId, true);
@@ -66,18 +66,26 @@ function afficherSelections(){
     let longueurTablePrestation = tablePrestation.length;
     var i = 1 ; //on ne parcours pas les entetes
     var ligne;
+    //affichage info stock
+    document.getElementById("warningStock").style.display = "none" ; 
+    document.getElementById("succesStock").style.display = "none" ;
 
-    var tableArticleSelectionne = [];
-    var tablePrestationSelectionne = [];
+    tableArticleSelectionne = [];
+    tablePrestationSelectionne = [];
 
     while(i<longueurTableArticle-1)
     {
         ligne = tableArticle[i];
         //on vérifie si quantité de la ligne est suppérieur à 0
-        if( ligne.cells[4].children[0].value > 0 )
+        if( ligne.cells[5].children[0].value > 0 )
         {
            tableArticleSelectionne.push(ligne);
            i++
+           //verifie si on a du stock
+           if(ligne.cells[5].children[0].value > ligne.cells[4].innerHTML)
+           {
+                document.getElementById("warningStock").style.display = "" ; //affichage info stock
+           }
         }
         else
         {
@@ -107,9 +115,34 @@ function afficherSelections(){
     }
 }
 
-function creerPDF()
+//creation de la facture
+function creerFacture()
 {
     afficherSelections();
+    ajusterStock();
+    //creerPDF();
+    chargerTableArticle();
+    chargerTablePrestation();
+}
+
+function ajusterStock()
+{
+    var xhr;
+    tableArticleSelectionne.forEach(function(ligne) 
+    { 
+        xhr = new XMLHttpRequest();
+        xhr.open("PUT", api+"articleStock", false);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send("id="+ligne.cells[0].innerHTML+"&salon_id="+salonId+"&stock="+Number(ligne.cells[4].innerHTML-ligne.cells[5].children[0].value));
+        if (xhr.status == 200 && xhr.readyState == 4 &&  xhr.responseText != "") {
+                document.getElementById("succesStock").style.display = "" ; //affichage info stock    
+        }
+    });     
+    document.getElementById("succesStock").style.display = "" ;
+}
+
+function creerPDF()
+{
     var doc = new jsPDF();
 
 
